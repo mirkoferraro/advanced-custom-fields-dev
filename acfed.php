f99c4cd31a4aedddd4a3036cf7289be1a72c80b8
Mirko Ferraro <mirko@MBP-di-Mirko.lan>
2016-07-23 22:26:54 +0200

    First commit

---
+++ acfed.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Advanced Custom Fields Easy Development
+Plugin URI: http://www.mirkoferraro.it
+Description: Makes the ACF registration via PHP easier
+Version: 1.0.0
+Author: Mirko Ferraro
+Author URI: http://www.mirkoferraro.it
+Copyright: Mirko Ferraro
+*/
